996562edd307ffacf5a4c0884b52a692bd653c41
YotrolZ <674317983@qq.com>
2017-03-24 20:36:08 +0800

    添加A_section

---
+++ A_section/A_section/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A_section
+//
+//  Created by Sunshine on 2017/3/24.
+//  Copyright © 2017年 Sunshine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
